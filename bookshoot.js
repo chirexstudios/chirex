f05c012e8706f0a9b35c073ab24c37bd70c6a2a3
sumitt-cloud <sumit.leonova1615@gmail.com>
2026-03-13 16:55:45 +0530

    fixing booking

--- bookshoot.js
+++ bookshoot.js
@@ -1,5 +1,35 @@
-fetch("./bookshoot.html")
-  .then((res) => res.text())
+fetch("bookshoot.html")
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("bookshoot.html not found");
+    }
+    return res.text();
+  })
   .then((data) => {
-    document.getElementById("bookshoot-section").innerHTML = data;
-  });
+    document.getElementById("bookshoot-container").innerHTML = data;
+
+    emailjs.init("YFP_ns1_d7QVwtmQj");
+
+    const form = document.querySelector("#book-form");
+
+    if (form) {
+      form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        emailjs.sendForm(
+          "Service_Chirex",
+          "template_5a1ckjp",
+          this
+        ).then(
+          () => {
+            alert("Inquiry Sent Successfully!");
+          },
+          (error) => {
+            console.log(error);
+            alert("Failed to send message");
+          }
+        );
+      });
+    }
+  })
+  .catch((err) => console.error(err));
